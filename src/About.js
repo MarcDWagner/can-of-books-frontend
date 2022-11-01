26a650ab0ec7a43673c5cfedf9dca0b1c9c15eb3
Marc D Wagner <marc.wagner@yahoo.com>
2022-11-01 07:39:10 -0700

    added about section with space for developer bio and names

--- src/About.js
+++ src/About.js
@@ -4,7 +4,16 @@ class Profile extends Component {
 
   render() {
     /* TODO: render information about the developers */
-    return <p>Profile page coming soon</p>
+    return (
+    <>
+    <h1>Meet the Developers:</h1>
+    <h2>Kenny Lino</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel turpis nunc eget lorem dolor sed viverra ipsum. Lobortis feugiat vivamus at augue eget arcu dictum varius duis.</p>
+    <h2>Marc Wagner</h2>
+    <p>I retired 2 years ago from the US Army and since then have been a project manager for a construction testing and inspection company.  I am a software developer because I enjoy problem-solving.  I specifically chose this field because it allows me to learn new things, always presents a challenge, and I will be able to grow professionally.  I am hoping to find a role in an environment where I can work with a small team and see projects from start to finish.  I have a lot of soft-skills leaders need to be effective; gained from years of managing and training increasingly larger groups.</p>
+    
+    </>
+    )
   }
 };
 
