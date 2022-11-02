72508b8575fbe330e884e1f24bd60f8565188a8e
Marc D Wagner <marc.wagner@yahoo.com>
2022-11-02 12:52:25 -0700

    set up update form

---
+++ src/UpdateBookForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+class UpdateBookForm extends React.Component {
+  render() {
+    return(
+      <Container>
+        
+      </Container>
+    )
+  }
+}
+
+export default UpdateBookForm;
